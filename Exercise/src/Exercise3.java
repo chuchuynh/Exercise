24da5d4f61e426eb3f13682cf5c358e9823c3a11
HuyLT <huylt@fsoft.com.vn>
2017-04-30 16:34:23 +0700

    InRaSoNgayCuaThang.java SoNguyenTo.java

--- Exercise/src/Exercise3.java
+++ Exercise/src/Exercise3.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
 
 public class Exercise3 {
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Nhap nam: ");
+		int year = input.nextInt();
+		if (year < 0) {
+			System.out.println("Nhap sai");
+			return;
+		}
+		if (laNamNhuanHayKo(year)) {
+			System.out.println("Day la Nam Nhuan");
+		} else {
+			System.out.println("Day khong phai la Nam Nhuan");
+		}
+	}
 
+	static boolean laNamNhuanHayKo(int year) {
+		return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
 	}
 }
