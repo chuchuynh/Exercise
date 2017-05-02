3c6400453e48e7fece12adcd1bf90635e0f377cf
Lê Trần Huy <huylt@azteam.vn>
2017-05-02 15:53:25 +0700

    update code

--- Exercise/src/InRaSoNgayCuaThang.java
+++ Exercise/src/InRaSoNgayCuaThang.java
@@ -1,7 +1,14 @@
 
 public class InRaSoNgayCuaThang {
+	private static final int INVALID_MONTH_DATE = -1;
+
 	public static void main(String[] args) {
-		soNgayCuaThang(-1, 2017);
+//		int songay = ngayCuaThang_Switch(10, 2017);
+//		if (songay == INVALID_MONTH_DATE) {
+//			System.out.println("Khong hop le");
+//		} else {
+//			System.out.println("So ngay cua thang la: " + songay);
+//		}
 		// soNgayCuaThang(2, 2017);
 		// soNgayCuaThang(3, 2017);
 		// soNgayCuaThang(4, 2017);
@@ -13,6 +20,12 @@ public class InRaSoNgayCuaThang {
 		// soNgayCuaThang(10, 2017);
 		// soNgayCuaThang(11, 2017);
 		// soNgayCuaThang(12, 2017);
+		ngayCuaThang_Switch1(-1, 2017);
+		ngayCuaThang_Switch1(9, -1);
+		ngayCuaThang_Switch1(9, 2017);
+		ngayCuaThang_Switch1(10, 2017);
+		ngayCuaThang_Switch1(11, 2017);
+		ngayCuaThang_Switch1(12, 2017);
 	}
 
 	static void soNgayCuaThang(int month, int year) {
@@ -31,6 +44,64 @@ public class InRaSoNgayCuaThang {
 			}
 		}
 
+	}
+
+	static int ngayCuaThang_Switch(int month, int year) {
+		switch (month) {
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			return 30;
+		case 2:
+			if (laNamNhuan(year))
+				return 29;
+			else
+				return 28;
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			return 31;
+		default:
+			return INVALID_MONTH_DATE;
+		}
+	}
+
+	static void ngayCuaThang_Switch1(int month, int year) {
+		if (((month > 12) || (month < 1)) || (year < 0)) {
+			System.out.println("Nhap sai Thang hoac Nam. Kiem tra lai");
+			return;
+		}
+		switch (month) {
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			System.out.println("Thang " + month + "; Nam: " + year + " co 30 ngay");
+			break;
+		case 2:
+			if (laNamNhuan(year)) {
+				System.out.println("Thang " + month + "; Nam: " + year + " co 29 ngay");
+			} else {
+				System.out.println("Thang " + month + "; Nam: " + year + " co 28 ngay");
+			}
+			break;
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			System.out.println("Thang " + month + "; Nam: " + year + " co 31 ngay");
+			break;
+		default:
+			break;
+		}
 	}
 
 	static boolean laNamNhuan(int year) {
